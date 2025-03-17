1252139accbd905bb66aca73aebaadd2d59d94d0
Ayush Aryal <ayusharyal100@gmail.com>
2025-03-17 14:33:37 +0545

    fix target name

--- ananeke/ananeke/doctype/employee_target/employee_target.js
+++ ananeke/ananeke/doctype/employee_target/employee_target.js
@@ -1,8 +1,30 @@
 // Copyright (c) 2025, Ayush Aryal and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Employee Target", {
-// 	refresh(frm) {
+frappe.ui.form.on("Employee Target", {
+    refresh: function(frm) {
+        // Your refresh logic here (if needed)
+    },
+    before_save: function(frm) {
+        if (frm.doc.employee) {
+            frappe.call({
+                method: 'frappe.client.get',
+                args: {
+                    doctype: 'Employee',
+                    name: frm.doc.employee
+                },
+                callback: function(r) {
+                    if (r.message) {
+                        var full_name = r.message.employee_name;
+                        console.log(typeof(full_name));
+                        frm.set_value('employee_name', full_name);
+                    }
+                }
+            });
+        }
+    }
+});
 
-// 	},
-// });
+
+
+
--- ananeke/ananeke/doctype/employee_target/employee_target.py
+++ ananeke/ananeke/doctype/employee_target/employee_target.py
@@ -3,7 +3,7 @@
 
 # import frappe
 from frappe.model.document import Document
-
+import frappe
 
 class EmployeeTarget(Document):
-	pass
+    pass
