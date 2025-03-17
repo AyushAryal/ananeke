1b0c6d9876f986ddb062dadf4d15eb36d855babc
Ayush Aryal <ayusharyal100@gmail.com>
2025-03-17 13:44:08 +0545

    add target

--- ananeke/hooks.py
+++ ananeke/hooks.py
@@ -169,7 +169,7 @@ doc_events = {
 		"on_submit": "ananeke.methods.sales_order.sales_order",
 	},
     "Sales Invoice": {
-	# 	"on_cancel": "ananeke.methods.sales_invoice.sales_invoice",
+		"on_cancel": "ananeke.methods.sales_invoice.sales_invoice",
 		"on_submit": "ananeke.methods.sales_invoice.sales_invoice",
 	# 	"before_save": "ananeke.methods.sales_invoice.sales_invoice",
 	},
