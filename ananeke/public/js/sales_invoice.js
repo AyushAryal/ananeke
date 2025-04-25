f5642910375dfa4970da1e1fe36e45e97ce91f5d
Ayush Aryal <ayusharyal100@gmail.com>
2025-04-25 16:33:10 +0545

    naming series for ret in sales invoice return updates automatically

--- ananeke/public/js/sales_invoice.js
+++ ananeke/public/js/sales_invoice.js
@@ -1,4 +1,12 @@
 frappe.ui.form.on("Sales Invoice", {
+    is_return: function(frm){
+        if (frm.doc.is_return){
+        frm.set_value("naming_series","ACC-SINV-RET-.YYYY.-");
+        }
+        else{
+        frm.set_value("naming_series","ACC-SINV-.YYYY.-")
+        }
+    },
     cost_center: function (frm) {
         if (frm.doc.cost_center) {
             frm.doc.items.forEach((item) => {
